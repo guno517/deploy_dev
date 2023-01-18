f472d2b48d04e2e9127c37f685ba18508d3fdf46
guno517 <guno5170@gmail.com>
2023-01-18 19:34:38 +0900

    server test

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,136 @@
-import { useEffect } from 'react';
-import api from '@/Api/api';
+import { useSetRecoilState } from 'recoil';
+import { InformLoginModal, InformLogOutModal } from '@/components/Modal';
+import { useEffect, useState } from 'react';
+import { categoryState } from '@/store/recoilCategoryState';
+import { Category, CategoryName, ReviewPosterType } from '@/types';
+import { getCategory } from '@/Api/category';
+import { getSpecifiedReviewPoster } from '@/Api/reviewPoster';
+import ReviewPoster from '@/components/Home/ReviewPoster';
+import CategoryList from '@/components/Home/Category/CategoryList';
+import { useNavigate } from 'react-router-dom';
 
-const API_URL = {
-  USER: {
-    // GET_USERS: 'api/users/get-users',
-    GET_USERS: '/users/get-users',
-  },
+type DataType = {
+  category: Category[];
+  specifiedPoster: {
+    id: string;
+    title: string;
+    image: string;
+  }[];
 };
 
+const validCategoryName: CategoryName[] = [
+  '노트북',
+  '모니터',
+  '시계',
+  '오디오',
+  '키보드',
+  '휴대폰',
+];
+
 const Home = () => {
+  const setCategory = useSetRecoilState(categoryState);
+  const [data, setData] = useState<DataType | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState(false);
+  const titleClassName =
+    'text-start sm:text-base md:text-lg lg:text-xl text-TEXT_BASE_BLACK font-semibold mb-2';
+
   useEffect(() => {
-    const start = async () => {
+    const run = async () => {
       try {
-        const response = await api.get(`/users/get-users`);
-        console.log(response);
+        const getAllMainData = (
+          await Promise.all([getCategory(), getSpecifiedReviewPoster()])
+        ).map(({ data }) => data);
+        const categoryResponse: Category[] = getAllMainData[0];
+        const specifiedReviewPosterResponse: {
+          id: string;
+          title: string;
+          image: string;
+        }[] = getAllMainData[1].map(
+          ({ _id, title, image }: Omit<ReviewPosterType, 'id'>) => ({
+            id: _id,
+            title,
+            image,
+          })
+        );
+        const validCategory = categoryResponse.filter((category) =>
+          validCategoryName.includes(category.name)
+        );
+        const validSpecifiedReviewPosterResponse = specifiedReviewPosterResponse.slice(
+          0,
+          2
+        );
+        const selectedCategory = validCategory.map(({ name, _id }) => ({
+          name,
+          id: _id,
+        }));
+
+        setData({
+          category: validCategory,
+          specifiedPoster: validSpecifiedReviewPosterResponse,
+        });
+        setCategory(selectedCategory);
       } catch (error) {
-        console.error(error);
+        if (error instanceof Error) {
+          setError(error);
+        }
+      } finally {
+        setLoading(false);
       }
     };
-    start();
+
+    run();
   }, []);
 
-  return (
-    <>
-      <div>test</div>
-    </>
-  );
+  const navigate = useNavigate();
+  const useLocationTest = () => {
+    // const userId = '63bf90f26c5dd32fa29e42d3';
+    const userId = '63c651e0eec0e34ad830a161';
+    navigate('/userpage', {
+      state: {
+        userId,
+      },
+    });
+  };
+
+  if (data && !loading) {
+    return (
+      <>
+        <button onClick={useLocationTest} className="btn">
+          uselocation
+        </button>
+        <h1
+          className={`md:hidden absolute top-5 left-1/2 -translate-x-1/2 text-5xl text-BASE font-extrabold`}
+        >
+          HIT
+        </h1>
+        <section className="flex flex-col items-center md:items-start lg:items-start max-w-xl w-full mx-auto pt-24 lg:pt-10 md:pt-10">
+          {/* 추천 게시글 area */}
+          <div className="w-11/12 h-full">
+            <h2 className={titleClassName}>추천 리뷰 게시글</h2>
+
+            <ReviewPoster
+              id={data?.specifiedPoster[0].id}
+              title={data?.specifiedPoster[0].title}
+              image={data?.specifiedPoster[0].image}
+            />
+            <ReviewPoster
+              id={data?.specifiedPoster[1].id}
+              title={data?.specifiedPoster[1].title}
+              image={data?.specifiedPoster[1].image}
+            />
+            {/* 카테고리 목록 area */}
+            <h2 className={titleClassName}>카테고리</h2>
+            <CategoryList category={data?.category} />
+          </div>
+        </section>
+        <InformLoginModal />
+        <InformLogOutModal />
+      </>
+    );
+  } else {
+    return <div>...Loading</div>;
+  }
 };
 
 export default Home;
